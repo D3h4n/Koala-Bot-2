1ba0937e62b55e26bbe12c0fe6c08b9d8f4dea8b
D3h4n <75465982+D3h4n@users.noreply.github.com>
2022-10-16 13:33:19 -0400

    Refactor executeCommand Function

--- src/interactions.ts
+++ src/interactions.ts
@@ -4,26 +4,42 @@ interface MessageReplier {
   reply: (string) => void
 }
 
+function echoCommand(options: Record<string, string | undefined>, replier: MessageReplier) {
+  const message = options['message'];
+  assert.ok(message, "ERROR: Echo command should always have message option")
+  assert.equal(
+      typeof message,
+      'string',
+      "ERROR: Message option for echo command should always be a string"
+  )
+  replier.reply(message)
+}
+
+function chooseCommand(options: Record<string, string | undefined>, replier: MessageReplier) {
+  const choices = Object
+      .entries(options)
+      .filter(([key, value]) => key.match(/choice[1-9]/) && typeof value === 'string')
+      .map((entry) => <string>entry[1])
+
+  assert.ok(choices.length >= 2, "ERROR: There should always be at least 2 choices")
+
+  const idx = Math.floor(Math.random() * choices.length)
+  replier.reply(`\`${choices[idx]}\``)
+}
+
 export function executeCommand(
     name: string, options: Record<string, string | undefined>, replier: MessageReplier
 ) {
-  if (name === "echo") {
-    assert.ok(options['message'], "ERROR: Echo command should always have message option")
-    assert.equal(
-      typeof options['message'],
-      'string',
-      "ERROR: Message option for echo command should always be a string"
-    )
-    replier.reply(options['message'])
-  }
+  switch (name) {
+    case "echo":
+      echoCommand(options, replier);
+      break
 
-  if (name === "choose") {
-    const choices = Object
-        .entries(options)
-        .filter(([key, value]) => key.match(/choice[1-9]/) && typeof value === 'string')
-        .map((entry) => <string>entry[1])
+    case "choose":
+      chooseCommand(options, replier);
+      break
 
-    const idx = Math.floor(Math.random() * choices.length)
-    replier.reply(`\`${choices[idx]}\``)
+    case "default":
+      break
   }
 }
