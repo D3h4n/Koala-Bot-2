0c868be770f0ca0f1f0e2c481f9dfc573905297a
D3h4n <75465982+D3h4n@users.noreply.github.com>
2022-10-16 11:41:46 -0400

    Add property based tests

--- tests/interactions.test.ts
+++ tests/interactions.test.ts
@@ -1,10 +1,9 @@
 import {handleCommand} from "../src/interactions";
+import * as fc from 'fast-check';
 
 describe("When receiving the echo command", () => {
-  it.each([
-    "Hello World", "Dance",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
-  ])("replies with the correct message", (message) => {
+  it("replies with the correct message", () => {
+    const message = "Hello World"
     const replier = {
       reply: jest.fn()
     }
@@ -14,6 +13,20 @@ describe("When receiving the echo command", () => {
 
     // Assert
     expect(replier.reply).toBeCalledWith(message)
+  })
+
+  it("always replies with the correct message", () => {
+    fc.assert(fc.property(fc.string({minLength: 1}), (message) => {
+      const replier = {
+        reply: jest.fn()
+      }
+
+      // Act
+      handleCommand("echo", {message}, replier)
+
+      // Assert
+      expect(replier.reply).toBeCalledWith(message)
+    }))
   })
 })
 
